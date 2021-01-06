1f104429d347f0be7af8cc9fc5b265832e1a3386
spurikutya <willisthewolfdog@gmail.com>
2021-01-06 10:59:25 +0100

    Add project files.

---
+++ MikRobi2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Reflection;
+
+
+namespace MikRobi2
+{
+    class Program
+    {
+        static string ProgramName = "ÉGvilág Mikrobi v" + Assembly.GetEntryAssembly().GetName().Version;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ProgramName);
+        }
+    }
+}
